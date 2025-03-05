e0412c2e7092c83171478dd23a844f9cf098e5b4
Popov Grigorii <59528040+PopovGP@users.noreply.github.com>
2025-03-05 23:46:11 +0300

    Update mikrotik-chr.sh

--- mikrotik-chr.sh
+++ mikrotik-chr.sh
@@ -10,12 +10,17 @@
 apt-get install -y curl unzip
 curl -O https://download.mikrotik.com/routeros/7.18.1/chr-7.18.1.img.zip
 unzip chr-7.18.1.img.zip
+
+echo u > /proc/sysrq-trigger
+
 dd if=chr-7.18.1.img of=/dev/vda bs=4M oflag=sync
+
 #
 # two lines below this comment are required
 # documentation: https://www.kernel.org/doc/html/v5.1/admin-guide/sysrq.html
 # enable sysrq
 echo 1 > /proc/sys/kernel/sysrq
+
 #  send command to sysrq
 # command 'b' - will immediately reboot the system without syncing or unmounting your disks.
 echo b > /proc/sysrq-trigger
